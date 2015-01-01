76fce306973ee824bc98f16de4cc91252619aa8c
Khanh Hoang <hoangchaukhanh93@gmail.com>
2015-01-01 23:07:23 +0700

    Init project Roll-a-Ball
    Chapter 01 - 04
    http://unity3d.com/learn/tutorials/projects/roll-a-ball/pickup-objects

---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControler : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position;
+        Debug.Log(transform.position);
+	}   
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Debug.Log(player.transform.position);
+        transform.position = player.transform.position + offset;
+	}
+}
